932f7d53feea97ecc279e5d871a679158b36b5a6
LyKhaiNghiep <khainghieply@gmail.com>
2025-03-18 14:53:18 +0700

    Add roleMapping to userTable

--- src/components/componentsAdmin/userTable/UserTable.jsx
+++ src/components/componentsAdmin/userTable/UserTable.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Table, Button } from "react-bootstrap";
 
+const roleMapping = {
+  1: "Admin",
+  2: "Therapist",
+  3: "Customer",
+};
+
 const UserTable = ({ users, onDelete, onEdit }) => {
   return (
     <Table striped bordered hover>
@@ -9,7 +15,7 @@ const UserTable = ({ users, onDelete, onEdit }) => {
           <th>ID</th>
           <th>Name</th>
           <th>Username</th>
-          <th>RoleID</th>
+          <th>Role</th>
           <th>Email</th>
           <th>Is active</th>
           <th>Actions</th>
@@ -23,7 +29,7 @@ const UserTable = ({ users, onDelete, onEdit }) => {
               {user.firstName} {user.lastName}
             </td>
             <td>{user.username}</td>
-            <td>{user.roleId}</td>
+            <td>{roleMapping[user.roleId]}</td>
             <td>{user.email}</td>
             <td>{user.isActive ? "Yes" : "No"}</td>
             <td>
